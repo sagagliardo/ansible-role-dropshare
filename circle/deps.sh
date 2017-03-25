305c180bd5171f3eafee4bd2b81bf651046bd705
Stephen Gagliardo <stephen.gagliardo@gmail.com>
2017-03-25 12:21:54 -0400

    fixed some syntax in deps.sh

--- circle/deps.sh
+++ circle/deps.sh
@@ -5,8 +5,8 @@ CIRCLECI_CACHE_DIR="${HOME}/bin"
 PACKER_URL="https://releases.hashicorp.com/terraform/0.9.1/terraform_0.9.1_linux_amd64.zip"
 
 if [ ! -f "${CIRCLECI_CACHE_DIR}/terraform" ]; then
-  wget -O /tmp/packer.zip "${PACKER_URL}"
-  unzip -d "${CIRCLECI_CACHE_DIR}" /tmp/packer.zip
+  wget -O /tmp/terraform.zip "${PACKER_URL}"
+  unzip -d "${CIRCLECI_CACHE_DIR}" /tmp/terraform.zip
 fi
 
 terraform version
