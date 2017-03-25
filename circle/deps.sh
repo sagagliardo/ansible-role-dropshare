6ebf342f763872e198e6666ca3a94fa09f0bae79
Stephen Gagliardo <stephen.gagliardo@gmail.com>
2017-03-25 12:04:36 -0400

    checking deps install correctly

---
+++ circle/deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+CIRCLECI_CACHE_DIR="${HOME}/bin"
+PACKER_URL="https://releases.hashicorp.com/terraform/0.9.1/terraform_0.9.1_linux_amd64.zip"
+
+if [ ! -f "${CIRCLECI_CACHE_DIR}/terraform" ]; then
+  wget -O /tmp/packer.zip "${PACKER_URL}"
+  unzip -d "${CIRCLECI_CACHE_DIR}" /tmp/packer.zip
+fi
+
+terraform version
---
+++ tests/test_default.py
@@ -0,0 +1,8 @@
+import testinfra
+
+def test_hosts_file(File):
+    f = File('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
