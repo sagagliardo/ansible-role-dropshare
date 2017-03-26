2b52c758b6a0915ed12889469ab35fe5bee133d9
Stephen Gagliardo <stephen.gagliardo@gmail.com>
2017-03-26 14:42:14 -0400

    revised playbook paths

--- circle/terraform/servers.tf
+++ circle/terraform/servers.tf
@@ -3,7 +3,7 @@ resource "digitalocean_droplet" "dropshare_test" {
   image = "centos-7-x64"
   region = "nyc3"
   size = "512mb"
-  ssh_keys = ["7582189"]
+  ssh_keys = ["${var.pubkey}"]
 
   provisioner "local-exec" {
     command = "ansible-playbook -i '${self.ipv4_address},' circle/playbook.yml -u root --extra-vars 'ip_address=${self.ipv4_address}'"
--- circle/terraform/variables.tf
+++ circle/terraform/variables.tf
@@ -1,1 +1,2 @@
 variable "do_token" {}
+variable "pubkey" {}
