948416ae99107109927a3b608785dbd241bf06a7
Stephen Gagliardo <stephen.gagliardo@gmail.com>
2017-03-26 20:44:22 -0400

    added logic to get and install pip on the test server

--- circle/deps.sh
+++ circle/deps.sh
@@ -2,6 +2,7 @@
 set -e
 
 CIRCLECI_CACHE_DIR="${HOME}/bin"
+### Get Terraform
 PACKER_URL="https://releases.hashicorp.com/terraform/0.9.1/terraform_0.9.1_linux_amd64.zip"
 
 if [ ! -f "${CIRCLECI_CACHE_DIR}/terraform" ]; then
@@ -10,3 +11,11 @@ if [ ! -f "${CIRCLECI_CACHE_DIR}/terraform" ]; then
 fi
 
 terraform version
+
+### Get pip installer script
+
+PIP_URL="https://bootstrap.pypa.io/get-pip.py"
+
+if [ ! -f "${CIRCLECI_CACHE_DIR}/get-pip.py" ]; then
+  wget -O "${CIRCLECI_CACHE_DIR}/get-pip.py" "${PIP_URL}"
+fi
